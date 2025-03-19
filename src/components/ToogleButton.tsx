38fa195546cb8e7a8ddb5c15fed5fff2ca07a387
Jean Lima <jeanlima5939@gmail.com>
2025-03-19 10:12:22 -0300

    Fix: Toogle Button Desing

--- src/components/ToogleButton.tsx
+++ src/components/ToogleButton.tsx
@@ -3,21 +3,20 @@
 import { Button } from "@/components/ui/button";
 import useThemeStore from "@/store/theme.store";
 import { useEffect } from "react";
+import { FaMoon } from "react-icons/fa6";
+import { MdSunny } from "react-icons/md";
 
-
-export default function ToogleButton() {
+export default function ToggleButton() {
   const initializeTheme = useThemeStore((state) => state.initializeTheme);
+  const { theme, toggleTheme } = useThemeStore();
 
   useEffect(() => {
-
     initializeTheme();
   }, [initializeTheme]);
-  const { theme, toggleTheme } = useThemeStore();
+
   return (
-    <>
-     <Button onClick={toggleTheme} className="p-2 bg-primary">
-      Alternar para {theme === "light" ? "escuro" : "claro"}
+    <Button onClick={toggleTheme} className="p-2 bg-background text-text flex items-center gap-2 hover:bg-background-80 transition-all duration-300">
+      {theme === "light" ? <FaMoon size={30} /> : <MdSunny size={30} />} 
     </Button>
-    </>
   );
 }
