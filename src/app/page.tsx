85db98d2f8261f41bdf38ce94a704af0cbafa4b7
Jean Lima <jeanlima5939@gmail.com>
2025-03-19 11:16:40 -0300

    Feat: Base Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="bg-background text-text flex items-center justify-center h-screen text-2xl">
+      Texto de Teste
     </div>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import LanguageToogle from "../language.toogle/LanguageButton";
+import ToggleButton from "../theme.toogle/ToogleButton";
+
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) { 
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`${
+        isScrolled ? "bg-transparent  backdrop-blur-lg" : "bg-transparent"
+      } text-text shadow-2xl flex items-center px-5 h-24 justify-between sticky top-0 left-0 w-full z-10`}
+    >
+      <div className="p-3 flex items-end">
+        <img src="/assets/logo/Logo.svg" alt="Logo" className="max-h-full max-w-[90px] object-contain" />
+        <img src="/assets/logo/LOADFORGE.svg" alt="Logo" className="max-h-full max-w-[100px] object-contain" />
+      </div>
+
+      <div className="flex gap-8 pr-24">
+        <LanguageToogle />
+        <ToggleButton />
+      </div>
+    </header>
+  );
+}
