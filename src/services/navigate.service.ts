4c0764bccfe2e290cc908d891d209edde51a818d
JeanLima2112 <jeanlima5939@gmail.com>
2025-04-13 23:12:09 -0300

    Fix: Redirect to a Hostgator Server

--- src/services/navigate.service.ts
+++ src/services/navigate.service.ts
@@ -1,11 +1,9 @@
-const EXTERNAL_URL = process.env.NEXT_PUBLIC_EXTERNAL_URL || "https://loadforge.github.io/SITE/";
+const EXTERNAL_URL = process.env.NEXT_PUBLIC_EXTERNAL_URL || "https://loadforge.online";
 
 export class ExternalService {
-
-
   static redirectToExternal(): void {
     if (typeof window !== "undefined") {
-      window.location.href = EXTERNAL_URL;
+      window.open(EXTERNAL_URL, "_blank");
     }
   }
 }
